53fbdbf594e973ac1bd824b01d31116fbda7a7b7
abrarfahim19 <fahimpatwari19@gmail.com>
2024-07-27 20:52:05 +0600

    Cleaned project

--- src/lib/markdown.ts
+++
@@ -1,7 +0,0 @@
-import { remark } from "remark";
-import html from "remark-html";
-
-export async function parseMarkdown(content: string): Promise<string> {
-  const processedContent = await remark().use(html).process(content);
-  return processedContent.toString();
-}
